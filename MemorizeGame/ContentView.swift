54ef0d7ab2c540e4394bc5c03d81439267959f38
demenkoeugene <mimosa.01-purgers@icloud.com>
2023-02-23 00:44:44 +0200

    Initial Commit

---
+++ MemorizeGame/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  MemorizeGame
+//
+//  Created by Eugene Demenko on 23.02.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MemorizeGame/MemorizeGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeGameApp.swift
+//  MemorizeGame
+//
+//  Created by Eugene Demenko on 23.02.2023.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
